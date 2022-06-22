386efb8efb9e5a1102c2176acf27711974561bf9
Ashton Tobar <+>
2022-06-22 13:15:34 -0500

    commit tree ebabe1f259a896891c9562507ef663c7d0b966af

--- test/index.js
+++ test/index.js
@@ -1,6 +1,89 @@
+// Optimize - 2 // 
+function hasTargetSum(array, target) {
+  const seenNumbers = {}
+
+  for(const number of array) {
+    const complement = target - number
+    if (complement in seenNumbers) return true
+    seenNumbers[number] = true
+  }
+  return false
+}
+
+
+// Optimize - 1 // 
+// function hasTargetSum(array, target) {
+//   const seenNumbers = {}
+
+//   for(let i = 0; i < array.length; i++) {
+//     console.log(array[i])
+//     const complement = target - array[i]
+//     if (seenNumbers[complement]) return true
+//     seenNumbers[array[i]] = true
+//   }
+//   return false
+// }
+
+// Original Function //
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     const difference = target - array[i]
+
+//     for (let j = i +1; j < array.length; j++){
+//       if(array[j] === difference) {
+//         return true
+//       }
+//     }
+//   }
+//   return false
+// }
+
+/* 
+  Write the Big O time complexity of your function here
+  0(n * n)/O(n^2)
+*/
+
+/* 
+  takes an argument of an array of numbers &
+  an argument with a target number
+
+  the function should return TRUE if any combo (pair) of numbers w/in 
+  the array & === the target number
+
+  example: 
+  array = [2, 3, 7, 15, 28] 
+  target number = 10
+  the function will return TRUE because 3 + 7 === 10 (the target number)
+  
+  else returns false
+*/
+
+/*
+  Add written explanation of your solution here
+  first we iterate over the array (i)
+  next we set a variable to find the difference between the target number & i 
+    => we set this to see if there is a number w/in the array that matches, if yes then
+    => that means i plus the number that matches the difference === the target number
+  then we need to iterate over the array again to see if there is a # (j) that matches difference
+  if j === difference then return true 
+    => iterate over the entire array
+  if none of the numbers in the array match the difference then return false 
+
+  ex: using first test below 
+    array = [3, 8, 12, 4, 11, 7]
+    target = 10
+
+  first iteration i = 3
+  the variable gets set: 10 (target) - 3 (i) = 7 (difference)
+  iterate through the array again to find j
+    => iterates through the entire array, until it hits the last #, 7 which matches
+    => returns true
+*/
+
+
 const chai = require("chai");
 global.expect = chai.expect;
-const hasTargetSum = require("../index");
+// const hasTargetSum = require("../index");
 
 describe("hasTargetSum", () => {
   it("returns true when the array is [3, 8, 12, 4, 11, 7] and the target is 10", () => {
